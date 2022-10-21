c03e4ba6126f4c53d3ff137ee6b152b185828a10
Dmitrii Nabok <d.nabok@fz-juelich.de>
2022-10-21 13:57:04 +0200

    Implementation of the original (measurement-based) and the projection operator-based QKS methods. * Both version work with both statevector and shots simulators. * Projection-based QKS has two implementation for commputing the   expectation values of the Pauli projection operators:   1) very fast statevector implementation that works only for a small      number of qubits (<14) due to intensive memory comsumption.   2) OpflowQNN implementation that supports circuit sampling and      therefore goes to a high number of qubits.

--- QuantumKitchenSinks.py
+++ QuantumKitchenSinks.py
@@ -78,10 +78,20 @@ class QuantumKitchenSinks:
                     embedding[i, j + k*self.n_episodes] = meas[k]
         return embedding
     
+from qiskit_machine_learning.neural_networks import OpflowQNN
+from qiskit.utils import QuantumInstance, algorithm_globals
+from qiskit import Aer
 
-class ProjectedQuantumKitchenSinksSV:
+# from qiskit import QuantumCircuit, execute
+# from qiskit.circuit import ParameterVector
+from qiskit.quantum_info.operators import Operator, Pauli
+from qiskit.opflow import AerPauliExpectation, StateFn, PauliSumOp, ListOp
+
+from qiskit.opflow import OperatorStateFn, CircuitStateFn
+
+class ProjectedQuantumKitchenSinks:
     
-    def __init__(self, n_features, fm, backend=None, episodes=10, stddev=2.0, seed=None, sampling='normal', projection='z'):
+    def __init__(self, n_features, fm, backend=None, episodes=10, stddev=2.0, seed=None, sampling='normal', projection='z', method='qnn'):
         """ """
         self.fm = fm
         self.n_features = n_features
@@ -90,18 +100,21 @@ class ProjectedQuantumKitchenSinksSV:
         self.stddev = stddev
         self.sampling = sampling
         self.projection = projection
+        self.method = method
         self.seed = seed
         self.backend = backend
         if self.backend is None:
             backend_options = {
-                'max_parallel_threads': 8,
+                'max_parallel_threads': 0,
                 'max_parallel_experiments': 1,
                 'max_memory_mb': 0,
                 'statevector_sample_measure_opt': 10,
             }
             algorithm_globals.random_seed = self.seed
             self.backend = QuantumInstance(
-                Aer.get_backend("statevector_simulator"), 
+                Aer.get_backend("aer_simulator_statevector"),
+                # Aer.get_backend("aer_simulator"),
+                shots=1024, backend_options=backend_options,
                 seed_simulator=seed, seed_transpiler=seed,
             )
         
@@ -116,19 +129,20 @@ class ProjectedQuantumKitchenSinksSV:
         # generate projection operators
         if isinstance(self.projection, str):
             if self.projection=='x':
-                self.proj_ops = self._o_x()
+                self.proj_ops = self._measurement_operator(['X'])
             elif self.projection=='y':
-                self.proj_ops = self._o_y()
+                self.proj_ops = self._measurement_operator(['Y'])
             elif self.projection=='z':
-                self.proj_ops = self._o_z()
+                self.proj_ops = self._measurement_operator(['Z'])
             elif self.projection=='xyz':
-                self.proj_ops = self._o_xyz()
+                self.proj_ops = self._measurement_operator(['X', 'Y', 'Z'])
             elif self.projection=='xyz_sum':
                 self.proj_ops = []
-                for x, y, z in zip(self._o_x(), self._o_y(), self._o_z()):
-                    self.proj_ops.append(x+y+z) 
-        else:
-            self.proj_ops = self.projection
+                for x, y, z in zip(_measurement_operator(['X']), _measurement_operator(['Y']), _measurement_operator(['Z'])):
+                    self.proj_ops.append([x, y, z])
+            else:
+                self.proj_ops = self.projection
+            print(self.proj_ops)
         
     def _get_omega_and_beta(self):
         mask = np.zeros((self.n_episodes, self.n_features, self.n_params))
@@ -151,221 +165,43 @@ class ProjectedQuantumKitchenSinksSV:
         self.beta  = np.random.uniform(0.0, 2*np.pi, (self.n_episodes, self.n_params))
         return
 
-    def _o_x(self):
+    def _measurement_operator(self, gates):
         s = 'I'*self.fm.num_qubits
         op = []
         for i in range(self.fm.num_qubits):
-            for gate in ['X']:
-                ss = list(s)
-                ss[-i-1] = gate
-                op.append(Operator(Pauli(''.join(ss))))
-        return op
-    
-    def _o_y(self):
-        s = 'I'*self.fm.num_qubits
-        op = []
-        for i in range(self.fm.num_qubits):
-            for gate in ['Y']:
-                ss = list(s)
-                ss[-i-1] = gate
-                op.append(Operator(Pauli(''.join(ss))))
-        return op
-    
-    def _o_z(self):
-        s = 'I'*self.fm.num_qubits
-        op = []
-        for i in range(self.fm.num_qubits):
-            for gate in ['Z']:
-                ss = list(s)
-                ss[-i-1] = gate
-                op.append(Operator(Pauli(''.join(ss))))
-        return op
-    
-    def _o_xyz(self):
-        s = 'I'*self.fm.num_qubits
-        op = []
-        for i in range(self.fm.num_qubits):
-            for gate in ['X', 'Y', 'Z']:
-                ss = list(s)
-                ss[-i-1] = gate
-                op.append(Operator(Pauli(''.join(ss))))
-        return op
-    
-    def projected_feature_map(self, x):
-        """ """
-        qc = QuantumCircuit(self.fm.num_qubits)
-
-        x_dict = dict(zip(self.fm.parameters, x))
-        psi_x = self.fm.assign_parameters(x_dict)
-        qc.append(psi_x.to_instruction(), qc.qubits)
-        result = self.backend.execute(qc)
-        sv = result.get_statevector(qc, decimals=4)
-    
-        ev = []
-        for op in self.proj_ops:
-            ev.append(np.real(sv.expectation_value(op)))
-            
-        del result, sv 
-        return ev
-        
-    def embedding(self, X):
-        """ Compute embedding """
-        params = X.dot(self.omega) + self.beta
-        # print('params: ', params.shape)
-        N = len(self.proj_ops) 
-        embedding = np.zeros((X.shape[0], N*self.n_episodes))
-        for i, param in enumerate(params):
-            for j, theta in enumerate(param):
-                meas = self.projected_feature_map(theta)
-                for k in range(N):
-                    embedding[i, j+k*self.n_episodes] = meas[k]
-        return embedding
-
-#################################################################################################
-#  Slower CircuitSampler version that was inspired by the Qiskit QNN titorial
-#################################################################################################
-from qiskit.opflow import CircuitSampler, AerPauliExpectation, StateFn, PauliSumOp, ListOp
-
-class ProjectedQuantumKitchenSinks:
-    
-    def __init__(self, n_features, fm, backend=None, episodes=10, stddev=2.0, seed=None, sampling='normal', projection='z'):
-        """ """
-        self.fm = fm
-        self.n_features = n_features
-        self.n_params = self.fm.num_parameters
-        self.n_episodes = episodes
-        self.stddev = stddev
-        self.sampling = sampling
-        self.projection = projection
-        self.seed = seed
-        self.backend = backend
-        if self.backend is None:
-            algorithm_globals.random_seed = self.seed
-            self.backend = QuantumInstance(
-                Aer.get_backend("statevector_simulator"), 
-                seed_simulator=seed, seed_transpiler=seed,
-            )
-        
-        self.split = int(self.n_features/self.n_params)
-        if self.split == 0:
-            assert False, "Inconsistent intput: split = 0! Check the number of parameters!"
-        
-        # Sampling parameters must be initialized only once!
-        self._get_omega_and_beta()
-        # print(self.omega)
-        
-        # generate projection operators
-        if isinstance(self.projection, str):
-            if self.projection=='x':
-                self.proj_ops = self._o_x()
-            elif self.projection=='y':
-                self.proj_ops = self._o_y()
-            elif self.projection=='z':
-                self.proj_ops = self._o_z()
-            elif self.projection=='xyz':
-                self.proj_ops = self._o_xyz()
-            elif self.projection=='xyz_sum':
-                self.proj_ops = self._o_xyz_sum()
-        else:
-            self.proj_ops = self.projection
-        
-    def _get_omega_and_beta(self):
-        mask = np.zeros((self.n_episodes, self.n_features, self.n_params))
-        for i in range(self.n_params):
-            j0 = i*self.split
-            j1 = min((i+1)*self.split, self.n_features)
-            mask[:, j0:j1, i] = 1.0
-        # print(mask)
-        np.random.seed(self.seed)
-        if self.sampling == 'normal':
-            omega = np.random.normal(0.0, self.stddev, (self.n_episodes, self.n_features))
-        elif self.sampling == 'uniform':
-            omega = np.random.uniform(-self.stddev, self.stddev, (self.n_episodes, self.n_features))
-        else:
-            assert False, "Unknown sampling type!"
-        self.omega = np.zeros((self.n_episodes, self.n_features, self.n_params))
-        for i in range(self.n_params):
-            self.omega[:, :, i] = omega
-        self.omega = mask * self.omega
-        self.beta  = np.random.uniform(0.0, 2*np.pi, (self.n_episodes, self.n_params))
-        return
-
-    def _o_x(self):
-        s = 'I'*self.fm.num_qubits
-        op = []
-        for i in range(self.fm.num_qubits):
-            op_s = []
-            for gate in ['X']:
-                ss = list(s)
-                ss[-i-1] = gate
-                op_s.append((''.join(ss), 1.0))
-            op.append(op_s)
-        return op
-    
-    def _o_y(self):
-        s = 'I'*self.fm.num_qubits
-        op = []
-        for i in range(self.fm.num_qubits):
-            op_s = []
-            for gate in ['Y']:
-                ss = list(s)
-                ss[-i-1] = gate
-                op_s.append((''.join(ss), 1.0))
-            op.append(op_s)
-        return op
-    
-    def _o_z(self):
-        s = 'I'*self.fm.num_qubits
-        op = []
-        for i in range(self.fm.num_qubits):
-            op_s = []
-            for gate in ['Z']:
-                ss = list(s)
-                ss[-i-1] = gate
-                op_s.append((''.join(ss), 1.0))
-            op.append(op_s)
-        return op
-    
-    def _o_xyz_sum(self):
-        s = 'I'*self.fm.num_qubits
-        op = []
-        for i in range(self.fm.num_qubits):
-            op_s = []
-            for gate in ['X', 'Y', 'Z']:
-                ss = list(s)
-                ss[-i-1] = gate
-                op_s.append((''.join(ss), 1.0))
-            op.append(op_s)
-        return op
-    
-    def _o_xyz(self):
-        s = 'I'*self.fm.num_qubits
-        op = []
-        for i in range(self.fm.num_qubits):
-            for gate in ['X', 'Y', 'Z']:
+            for gate in gates:
                 ss = list(s)
                 ss[-i-1] = gate
                 op.append([(''.join(ss), 1.0)])
         return op
-    
+            
     def projected_feature_map(self, x):
         """ """
-        qc = QuantumCircuit(self.fm.num_qubits)
-
-        x_dict = dict(zip(self.fm.parameters, x))
-        psi_x = self.fm.assign_parameters(x_dict)
-        qc.append(psi_x.to_instruction(), qc.qubits)
-        # print(qc.decompose().draw())
-        
-        op = []
-        for proj_op in self.proj_ops:
-            op.append( ~StateFn(PauliSumOp.from_list(proj_op)) @ StateFn(qc) )
-        op = ListOp(op)
-    
-        expectation = AerPauliExpectation().convert(op.reduce())
-        sampler = CircuitSampler(self.backend).convert(expectation)
-        return sampler.eval()
-        
+        if self.method=='statevector':
+            qc = self.fm.assign_parameters(x)
+            result = self.backend.execute(qc)
+            sv = result.get_statevector()
+            ev = []
+            for op in self.proj_ops:
+                o = Operator(Pauli(op[0][0]))
+                if len(op)>1:
+                    for i in range(1, len(op)):
+                        o = o + Operator(Pauli(op[i][0]))
+                ev.append(np.real(sv.expectation_value(o)))
+        elif self.method=='qnn':
+            fm_sfn = StateFn(self.fm)
+            list_ops = []
+            for op in self.proj_ops:
+                op = ~StateFn(PauliSumOp.from_list(op)) @ fm_sfn
+                list_ops.append(op)
+            list_ops = ListOp(list_ops)
+            expval = AerPauliExpectation()
+            qnn = OpflowQNN(list_ops, input_params=self.fm.parameters, weight_params=[], exp_val=expval, gradient=None, quantum_instance=self.backend)
+            ev = qnn.forward(x, []).flatten()
+        else:
+            assert False, 'Unknown method!'
+        return ev
+            
     def embedding(self, X):
         """ Compute embedding """
         params = X.dot(self.omega) + self.beta
@@ -378,5 +214,3 @@ class ProjectedQuantumKitchenSinks:
                 for k in range(N):
                     embedding[i, j+k*self.n_episodes] = meas[k]
         return embedding
-    
-
